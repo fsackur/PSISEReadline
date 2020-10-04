6bf1f47319b2b657d641004cf9e9b70417743379
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-04 23:07:40 +0100

    Separate out Register-PSISEReadlineShortcut

--- PSISEReadline.psm1
+++ PSISEReadline.psm1
@@ -81,43 +81,4 @@ Register-ArgumentCompleter -CommandName bck-i-search -ParameterName SearchString
 #endregion Tab-complete from history
 
 
-
-#region Bind ISE shortcut
-$Shortcut = "Ctrl+E"
-$MenuItemName = "bck-i-search"
-# place text in the input buffer, ready for tab-completion
-$Action = {
-    $psISE.CurrentPowerShellTab.ConsolePane.InputText = "bck-i-search "
-}
-
-
-# Clear existing entries of our command
-$SubMenus = $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus
-[void]$SubMenus.Where({$_.DisplayName -eq $MenuItemName}).ForEach({$SubMenus.Remove($_)})
-
-
-# We need a menu entry to bind a shortcut
-try
-{
-    [void]$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(
-        $MenuItemName,
-        $Action,
-        $Shortcut
-    )
-}
-catch
-{
-    if ($_ -match "uses shortcut '.*', which is already in use")
-    {
-        Write-Warning "Failed to bind '$Shortcut' to bck-i-search; already in use."
-    }
-    else
-    {
-        throw
-    }
-}
-
-# Cleanup, since we're expecting to be dot-sourced
-Remove-Variable Action, Shortcut, MenuItemName, SubMenus
-
-#endregion Bind ISE shortcut
+Get-ChildItem $PSScriptRoot\Public\*.ps1 | ForEach-Object {. $_.FullName}
---
+++ Public/Register-PSISEReadlineShortcut.ps1
@@ -0,0 +1,36 @@
+function Register-PSISEReadlineShortcut
+{
+    $Shortcut = "Ctrl+E"
+    $MenuItemName = "bck-i-search"
+    # place text in the input buffer, ready for tab-completion
+    $Action = {
+        $psISE.CurrentPowerShellTab.ConsolePane.InputText = "bck-i-search "
+    }
+
+
+    # Clear existing entries of our command
+    $SubMenus = $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus
+    [void]$SubMenus.Where({$_.DisplayName -eq $MenuItemName}).ForEach({$SubMenus.Remove($_)})
+
+
+    # We need a menu entry to bind a shortcut
+    try
+    {
+        [void]$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(
+            $MenuItemName,
+            $Action,
+            $Shortcut
+        )
+    }
+    catch
+    {
+        if ($_ -match "uses shortcut '.*', which is already in use")
+        {
+            Write-Warning "Failed to bind '$Shortcut' to bck-i-search; already in use."
+        }
+        else
+        {
+            throw
+        }
+    }
+}
