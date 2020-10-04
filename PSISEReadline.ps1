88633fdbae8e9a9ff1b8768b76eb37e8c820f6ef
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-04 21:11:16 +0100

    Tab-complete from history

--- PSISEReadline.ps1
+++ PSISEReadline.ps1
@@ -13,6 +13,35 @@ function bck-i-search
 }
 
 
+#region Tab-complete from history
+Register-ArgumentCompleter -CommandName bck-i-search -ParameterName Command -ScriptBlock {
+    param
+    (
+        $commandName,
+        $parameterName,
+        $wordToComplete,
+        $commandAst,
+        $fakeBoundParameters
+    )
+
+    [string[]]$History = Get-History |
+        sort Id -Descending |
+        select -ExpandProperty CommandLine -First 1000
+
+    # exclude self
+    $History = $History -notmatch '^bck-i-search'
+
+    # Deduplicate - this relies on HashSet.Add returning False when item already in set
+    [System.Collections.Generic.HashSet[string]]$UniqueHistory = @()
+    $History = $History | where {$UniqueHistory.Add($_)}
+
+    # return matching
+    $History -like "*$wordToComplete*"
+}
+#endregion Tab-complete from history
+
+
+
 #region Bind ISE shortcut
 $Shortcut = "Ctrl+E"
 $MenuItemName = "bck-i-search"
