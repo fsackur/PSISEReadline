d66a1b9fa9352a1d07400a3ad683bcd490983141
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-14 22:41:37 +0100

    It gets the most recent command!

--- scratch.ps1
+++ scratch.ps1
@@ -5,13 +5,17 @@ function bck-i-search
         Unregister-Event -SourceIdentifier PaneChanged
         
         # $psISE.CurrentPowerShellTab.ConsolePane
-        $Pane = $args[0]
-        $PaneType = $Pane.GetType()
-        $Field = $PaneType.GetField('inputTextBeforeExecution', 'nonpublic,instance')
-        $InputTextBeforeExecution = $Field.GetValue($Pane)
+        if (-not $SearchString)
+        {
+            $Pane = $args[0]
+            $PaneType = $Pane.GetType()
+            $Field = $PaneType.GetField('inputTextBeforeExecution', 'nonpublic,instance')
+            $InputTextBeforeExecution = $Field.GetValue($Pane)
+            $SearchString = $InputTextBeforeExecution
+        }
         
-        #$Pane.InputText = ((Get-History).CommandLine -like "*$inputTextBeforeExecution*" | Select -Last 1) #-join "`r`n"
-        Write-host ((Get-History).CommandLine -like "*$inputTextBeforeExecution*" | Select -Last 1) -ForegroundColor Green
+        $Pane.InputText = (Get-History).CommandLine -like "*$SearchString*" | Select -Last 1
+        #Write-host ((Get-History).CommandLine -like "*$SearchString*" | Select -Last 1) -ForegroundColor Green
     }
 
     $null = Register-ObjectEvent -InputObject $psISE.CurrentPowerShellTab.ConsolePane -EventName PropertyChanged -Action $Action -MaxTriggerCount 1 -SourceIdentifier PaneChanged
