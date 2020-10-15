442cd7a35fd29fdaba52b71f26c1de2ab63e7e5c
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-15 08:52:21 +0100

    New tack! Nested module that properly implements event-handling

---
+++ handler.psm1
@@ -0,0 +1,23 @@
+﻿$Script:var = 2
+<#
+$EventSplat = @{
+    InputObject = $psISE.CurrentPowerShellTab.ConsolePane
+    EventName = 'PropertyChanged' 
+    Action = {handlePropertyChanged $args[0] $args[1]}
+    MaxTriggerCount = 1 
+    SourceIdentifier = 'PaneChanged'
+}
+Register-ObjectEvent @EventSplat
+#>
+
+function handlePropertyChanged
+{
+    param
+    (
+        $Sender,
+        $EventArgs
+    )
+    Write-Host $MyInvocation.MyCommand.Module.Name
+    Write-Host $EventArgs
+    Write-Host $Var
+}
