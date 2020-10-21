c35fb1edb60d34ee56b75616b010ca8a33cc8c02
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-21 02:14:50 +0100

    Import Open.WinKeyboardHook library

--- PSISEReadline.psm1
+++ PSISEReadline.psm1
@@ -1,4 +1,32 @@
 
+
+try
+{
+    $Global:HOOK = [Open.WinKeyboardHook.KeyboardInterceptor]::new()
+}
+catch
+{
+    if ($_.FullyQualifiedErrorId -ne 'TypeNotFound')
+    {
+        throw
+    }
+
+    $Package = Get-Package Open.WinKeyboardHook
+    if (-not $Package)
+    {
+        throw "Required package 'Open.WinKeyboardHook' is missing. To install, run 'Install-Package Open.WinKeyboardHook'."
+    }
+
+    $DllPath = $Package.Source |
+        Split-Path |
+        Join-Path -ChildPath lib |
+        Join-Path -ChildPath net40 |
+        Join-Path -ChildPath Open.WinKeyboardHook.dll
+
+    Add-Type -Path $DllPath
+}
+
+
 Get-ChildItem $PSScriptRoot\Private\*.ps1 | ForEach-Object {. $_.FullName}
 Get-ChildItem $PSScriptRoot\Public\*.ps1  | ForEach-Object {. $_.FullName}
 
