0dca80a2ca5760e37aba95876b89575b3efda8c2
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-21 02:15:02 +0100

    Fun with hooking the keyboard

--- Public/Invoke-PSISEReadline.ps1
+++ Public/Invoke-PSISEReadline.ps1
@@ -20,9 +20,47 @@ function Invoke-PSISEReadline
     $PaneType = $ConsolePane.GetType()
     $Field = $PaneType.GetField('inputTextBeforeExecution', 'nonpublic,instance')
     $InputTextBeforeExecution = $Field.GetValue($ConsolePane)
-    $SearchString = $InputTextBeforeExecution
+    $Global:_BCK_I_SEARCH = $InputTextBeforeExecution
 
+    $Action = {
+        # https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.keyeventargs?view=netcore-3.1
+        $KeyData = $EventArgs.KeyData
 
+        if ($Global:Debug)
+        {
+            $EventArgs | ft | Out-string | Write-Host -ForegroundColor Yellow -NoNewline
+        }
 
-    break
+        
+        $Char = $ShouldExit = $ShouldInvoke = $null
+        switch ($KeyData)
+        {
+            'C, Control' {$ShouldExit = $true; break}
+            'Escape' {$ShouldExit = $true; break}
+            'Return' {$ShouldExit = $true; $ShouldInvoke = $true; break}
+            'left'   {$ShouldExit = $true; break}
+            'right' {$ShouldExit = $true; break}
+            'up'     {$ShouldExit = $true; break}
+            'down'    {$ShouldExit = $true; break}
+            'back'  {$Global:_BCK_I_SEARCH = $Global:_BCK_I_SEARCH.Substring(0, ($Global:_BCK_I_SEARCH-1)); break}
+            Default {$Global:_BCK_I_SEARCH += $Char = $_.ToString().ToLower()}
+        }
+
+        if ($ShouldExit)
+        {
+            $psISE.CurrentPowerShellTab.ConsolePane.InputText = ""
+            Unregister-Event -SourceIdentifier 'bck-i-search' -ErrorAction SilentlyContinue
+            $prompt
+        }
+        else
+        {
+            $psISE.CurrentPowerShellTab.ConsolePane.InputText = "bck-i-search: $Global:_BCK_I_SEARCH"
+        }
+        # $Char | Write-Host -ForegroundColor Green -NoNewline
+    }
+
+    
+    Unregister-Event -SourceIdentifier 'bck-i-search' -ErrorAction SilentlyContinue
+    $null = Register-ObjectEvent -InputObject $HOOK -EventName KeyDown -SourceIdentifier 'bck-i-search' -MaxTriggerCount 6 -Action $Action
+    $HOOK.StartCapturing()
 }
